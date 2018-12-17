46decb4776c628c13c8b616824a6bd28ae6fba29
Jacobanna <pieprzyk.jakub@gmail.com>
2018-12-18 00:46:52 +0200

    Spring MVC

---
+++ springmvc/src/main/java/com/bharath/spring/springmvc/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.bharath.spring.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public ModelAndView hello() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("hello");
+        return modelAndView;
+    }
+}
---
+++ springmvc/src/main/webapp/WEB-INF/views/hello.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Jacob
+  Date: 18.12.2018
+  Time: 00:13
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Hello</title>
+</head>
+<body>
+<H1>Hello from Spring MVC!</H1>
+</body>
+</html>
